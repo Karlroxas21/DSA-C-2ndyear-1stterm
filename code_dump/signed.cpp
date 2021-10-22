2453af920e83784b5c2bb921e2317370e1bb9278
[Karl Roxas] <karlmarxroxas1@gmail.com>
2021-10-22 16:34:10 +0800

    update

--- code_dump/signed.cpp
+++ code_dump/signed.cpp
@@ -6,7 +6,7 @@ int main (){
     unsigned int uns2= -1; /*when unsigned, it will overflow. 
                             this is because now the int i doesn't have a sign 
                             */
-    // cout << uns1 << ", " << uns2;
+    cout << uns1 << ", " << uns2;
     
     return 0;
 }
